7e4716ad49401f7860179d01d1d1ac0ca4e259ff
Marcelo Mastroiani <mastroianimarcelo04@gmail.com>
2025-08-12 23:35:07 -0300

    feat: integrate NATS microservices for payment session handling
    - Added NATS microservices support by updating package.json to include @nestjs/microservices and nats.
    - Enhanced environment variable configuration to include NATS server addresses.
    - Updated main.ts to connect the application as a NATS microservice and start all microservices.
    - Modified PaymentsController to handle payment session creation via NATS message pattern instead of HTTP POST.

--- src/main.ts
+++ src/main.ts
@@ -2,7 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { Logger, ValidationPipe } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { envs } from './config';
-import * as bodyParser from 'body-parser';
+import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 
 async function bootstrap() {
   const logger = new Logger('Payments-ms');
@@ -19,6 +19,20 @@ async function bootstrap() {
       forbidNonWhitelisted: true,
     }),
   );
+
+  app.connectMicroservice<MicroserviceOptions>(
+    {
+      transport: Transport.NATS,
+      options: {
+        servers: envs.natsServers,
+      },
+    },
+    {
+      inheritAppConfig: true,
+    },
+  );
+
+  await app.startAllMicroservices();
 
   await app.listen(envs.port);
 
